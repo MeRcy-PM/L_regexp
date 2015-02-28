65f3562b351ea6749b7f7f3ebafa7c4ff121f5fe
MeRcy-PM <240560351@qq.com>
2015-02-28 14:00:29 +0800

    del C

---
+++ base.h
@@ -0,0 +1,12 @@
+#ifndef __BASE_H__
+#define __BASE_H__
+#include <iostream>
+#include <string>
+#include <vector>
+#include <assert.h>
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+
+#endif
